33de4924ff42d550d3ae3c8b4598578a6600a036
root <josephbkarage@gmail.com>
2019-10-07 15:18:24 +0300

    start running postgres server commit

--- bucketlist-api/start_postgres.sh
+++ bucketlist-api/start_postgres.sh
@@ -3,21 +3,18 @@ set -e
 
 echo "Waiting for postgres..."
 
-while ! nc -z amity-db 5432; do
+while ! nc -z database_service 5432; do
   sleep 0.1
 done
 
 echo "PostgreSQL started"
 
-python manage.py db init
-python manage.py db migrate
-python manage.py db upgrade
 
 python run.py
 
-# this bash script checks port number 5432 of container named amity-db
+# this bash script checks port number 5432 of container named test-db
 # to ascertain that it is open. this means that the postgres server is up and running
-# amity-db is what i named the postgres database container.
+# test-db is what i named the postgres database container.
 # when the port is open, then the script run all other subsequent commands following
 # which commands are essentially running migrations on the database in that said container
 # and then start the flask API application.
